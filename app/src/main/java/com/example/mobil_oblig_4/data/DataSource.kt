a395e1ff30cc5d41046c173ee9f9d318e2bbacbb
Andreas <anjoh7871@uit.no>
2026-04-15 10:19:31 +0200

    test

--- app/src/main/java/com/example/mobil_oblig_4/data/DataSource.kt
+++ app/src/main/java/com/example/mobil_oblig_4/data/DataSource.kt
@@ -40,7 +40,7 @@ import com.example.mobil_oblig_4.model.Brand
 class DataSource {
     val brands = listOf<Brand>(
         Brand(
-            name = "Trek",
+            name = "Tre",
             country = "USA"
         ),
         Brand(
